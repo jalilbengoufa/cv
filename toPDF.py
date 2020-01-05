e725b1ebfe658666ae5e93ff1a06eb0e83653037
jalilbengoufa <jalilbengoufa@gmail.com>
2020-01-05 18:21:52 -0500

    update fr CV

---
+++ toPDF.py
@@ -0,0 +1,15 @@
+import pdfkit 
+from PyPDF2 import PdfFileMerger
+
+
+pdfkit.from_file('index-page-1-fr.html', 'cv-1-fr.pdf')
+pdfkit.from_file('index-page-2-fr.html', 'cv-2-fr.pdf')
+cvPdfPages = ['cv-1-fr.pdf', 'cv-2-fr.pdf']
+
+merger = PdfFileMerger()
+
+for pdf in cvPdfPages:
+    merger.append(pdf)
+
+merger.write("cv-fr.pdf")
+merger.close()
