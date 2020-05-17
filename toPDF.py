94a605df37c58e451310e877701bb1856b0a1da8
jalil <jalilbengoufa@gmail.com>
2020-05-17 14:12:50 -0400

    update projets section and add github url

--- toPDF.py
+++ toPDF.py
@@ -6,10 +6,10 @@ pdfkit.from_file('index-page-1-fr.html', 'cv-1-fr.pdf')
 pdfkit.from_file('index-page-2-fr.html', 'cv-2-fr.pdf')
 cvPdfPages = ['cv-1-fr.pdf', 'cv-2-fr.pdf']
 
-merger = PdfFileMerger()
+merger = PdfFileMerger(strict=False)
 
 for pdf in cvPdfPages:
     merger.append(pdf)
 
-merger.write("cv-fr.pdf")
+merger.write("cv-fr.pdf",)
 merger.close()
