c0c6b3bded46620c6ba78d07fb69269617fa2d39
jalil <jalilbengoufa@gmail.com>
2020-05-27 19:21:10 -0400

    add english version not completed and index for github pages

--- script.sh
+++ script.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 inotifywait -e close_write,moved_to,create -m . |
 while read -r directory events filename; do
-  if [ "$filename" = "index-page-1-fr.html" ]; then
-    python toPDF.py
-  fi
-  if [ "$filename" = "index-page-2-fr.html" ]; then
+  if [ "${filename##*.}" == "html" ]; then
     python toPDF.py
   fi
 done
--- toPDF.py
+++ toPDF.py
@@ -4,12 +4,23 @@ from PyPDF2 import PdfFileMerger
 
 pdfkit.from_file('index-page-1-fr.html', 'cv-1-fr.pdf')
 pdfkit.from_file('index-page-2-fr.html', 'cv-2-fr.pdf')
-cvPdfPages = ['cv-1-fr.pdf', 'cv-2-fr.pdf']
+pdfkit.from_file('index-page-1-en.html', 'cv-1-en.pdf')
+pdfkit.from_file('index-page-2-en.html', 'cv-2-en.pdf')
+cvPdfPagesFr = ['cv-1-fr.pdf', 'cv-2-fr.pdf']
+cvPdfPagesEn = ['cv-1-en.pdf', 'cv-2-en.pdf']
 
-merger = PdfFileMerger(strict=False)
+mergerFr = PdfFileMerger(strict=False)
 
-for pdf in cvPdfPages:
-    merger.append(pdf)
+for pdf in cvPdfPagesFr:
+    mergerFr.append(pdf)
+    
+mergerFr.write("Jalil-Bengoufa-cv-fr.pdf",)
+mergerFr.close()
 
-merger.write("cv-fr.pdf",)
-merger.close()
+mergerEn = PdfFileMerger(strict=False)
+
+for pdf in cvPdfPagesEn:
+    mergerEn.append(pdf)
+
+mergerEn.write("Jalil-Bengoufa-cv-en.pdf",)
+mergerEn.close()
